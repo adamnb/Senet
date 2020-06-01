5096e803295fc891809926158a7a6c7382b49ccf
Adam Brown <adamnathanielbrown@gmail.com>
2020-05-31 20:54:33 -0700

    Added class that stores ansi color constants. WIP

---
+++ src/brown/adamn/AnsiColors.java
@@ -0,0 +1,9 @@
+package brown.adamn;
+
+public class AnsiColors {
+    public static final String A_RST = "\u001B[0m";
+
+    public static final String A_INP = "\u001B[33m";
+    public static final String A_GUARD1 = "\u001B[41m";
+    public static final String A_GUARD2 = "\u001b[44";
+}
